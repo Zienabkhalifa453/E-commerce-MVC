88d49dcab6437d73510d597c44fa05acd8dfb7e9
saraalshref <saraelshref12@gmail.com>
2024-03-31 20:57:59 +0200

    layout

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,10 +1,15 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_commerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_commerce_MVC.Controllers
 {
+
     public class CategoryController : Controller
     {
+
         public IActionResult GetAllCategory()
+
+
         {
             return View();
         }
